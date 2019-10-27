81816e7701921b2a091e185ac5d444cb97a6d3e5
N1ck <cbnickp@gmail.com>
2019-10-27 21:02:13 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  if (document.querySelectorAll('.pagination-links').length > 1) {
+    const paginationLinks = document.querySelectorAll('.pagination-links');
+    const slicedPath = window.location.pathname.slice(12, window.location.pathname.length);
+
+    for (let i=0; i<paginationLinks.length; i++) {
+      if (paginationLinks[i].textContent === slicedPath) {
+        paginationLinks[i].classList.add('active');
+      }
+    }
+
+  }
+}
